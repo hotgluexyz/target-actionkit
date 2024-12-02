197a0222493486608c08a4a194f6ab90211c252a
arilton <arilton@hotglue.io>
2024-12-02 17:11:50 -0300

    fix target and use lists

--- target_actionkit/client.py
+++ target_actionkit/client.py
@@ -20,6 +20,8 @@ class ActionKitSink(HotglueSink):
     ) -> None:
         super().__init__(target, stream_name, schema, key_properties)
         self.__auth = ActionKitAuth(dict(self.config))
+        self.lists = None
+        self.initialize_lists()
 
     @property
     def base_url(self):
@@ -33,7 +35,8 @@ class ActionKitSink(HotglueSink):
         msg = self.response_error_message(response)
         if hasattr(response, "text") and response.text:
             msg = f"{msg}. Response: {response.text}"
-
+        if response.status_code in [409]:
+            msg = f"{msg}. reason: {response.reason}"
         if response.status_code in [429] or 500 <= response.status_code < 600:
             raise RetriableAPIError(msg, response)
         elif 400 <= response.status_code < 500:
@@ -47,3 +50,24 @@ class ActionKitSink(HotglueSink):
             "Accept": "application/json",
             "Authorization": self.__auth(requests.Request())
         }
+    
+    def initialize_lists(self):
+        if getattr(self, "lists"):
+            return
+        self.lists = []
+        list_url = f"list/"
+        params = "?_limit=100"
+        next_url = f"{list_url}{params}"
+
+        while next_url:
+            response = self.request_api("GET", endpoint=next_url, headers=self.prepare_request_headers())
+            response_data = response.json()
+            self.lists.extend(response_data.get("objects", []))
+            params: str = response_data.get("meta", {}).get("next", "")
+            if params:
+                params = params.split("/")[-1]
+                next_url = f"{list_url}{params}"
+            else:
+                next_url = None
+        
+        self.map_list_name_to_id = {l["name"]: l["id"] for l in self.lists}
