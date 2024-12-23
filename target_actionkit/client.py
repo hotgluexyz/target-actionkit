f71161b822b022e7aa72b075a8a19fbbd4a2f64b
José Arilton Pereira Filho <arilton.japf@gmail.com>
2024-12-23 19:13:29 -0300

    improved validate_response (#2)

--- target_actionkit/client.py
+++ target_actionkit/client.py
@@ -27,18 +27,15 @@ class ActionKitSink(HotglueSink):
 
     def validate_response(self, response: requests.Response) -> None:
         """Validate HTTP response."""
-        if response.status_code in [409]:
-            msg = response.reason
-            raise FatalAPIError(msg)
-        elif response.status_code in [429] or 500 <= response.status_code < 600:
-            msg = self.response_error_message(response)
+        msg = self.response_error_message(response)
+        if hasattr(response, "text") and response.text:
+            msg = f"{msg}. Response: {response.text}"
+
+        if response.status_code in [429] or 500 <= response.status_code < 600:
             raise RetriableAPIError(msg, response)
         elif 400 <= response.status_code < 500:
-            try:
-                msg = response.text
-            except:
-                msg = self.response_error_message(response)
             raise FatalAPIError(msg)
+
 
     def prepare_request_headers(self):
         """Prepare request headers."""
